c602f5237d57184e4e2f45a859e0381dcfed3d4d
Solarin-Johnson <solarinjohnson@gmail.com>
2025-09-03 18:46:19 +0100

    feat: refactor Status component to use ThemedView and enhance Layout with transition support and header options

--- app/wa-status/[id].tsx
+++ app/wa-status/[id].tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function Status() {
   return (
-    <View>
+    <ThemedView colorName="text" style={{ flex: 1 }}>
       <Text>Status</Text>
-    </View>
+    </ThemedView>
   );
 }
--- app/wa-status/_layout.tsx
+++ app/wa-status/_layout.tsx
@@ -1,26 +1,39 @@
 import { ThemedText, ThemedTextWrapper } from "@/components/ThemedText";
-import { Stack, useNavigation } from "expo-router";
+import { useNavigation, withLayoutContext } from "expo-router";
 import Feather from "@expo/vector-icons/Feather";
 import { BlurView } from "expo-blur";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, useWindowDimensions, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { ChevronLeft } from "lucide-react-native";
+import type {
+  ParamListBase,
+  StackNavigationState,
+} from "@react-navigation/native";
+import Transition, {
+  createNativeStackNavigator,
+  type NativeStackNavigationEventMap,
+  type NativeStackNavigationOptions,
+} from "react-native-screen-transitions";
+import { interpolate, useSharedValue } from "react-native-reanimated";
+
+const { Navigator } = createNativeStackNavigator();
+
+export const Stack = withLayoutContext<
+  NativeStackNavigationOptions,
+  typeof Navigator,
+  StackNavigationState<ParamListBase>,
+  NativeStackNavigationEventMap
+>(Navigator);
 
 export default function Layout() {
   const bg = useThemeColor("waBg");
+
   return (
     <Stack
       screenOptions={{
         headerShown: false,
-        headerLeft: () => <HeaderLeft />,
-        headerRight: () => <HeaderRight />,
-        headerTransparent: true,
-        headerBackground: () => (
-          <BlurView style={StyleSheet.absoluteFill} intensity={60} />
-        ),
-        contentStyle: { backgroundColor: bg },
       }}
     >
       <Stack.Screen
@@ -28,6 +41,70 @@ export default function Layout() {
         options={{
           title: "My Status",
           headerShown: true,
+          headerLeft: () => <HeaderLeft />,
+          headerRight: () => <HeaderRight />,
+          headerBackground: () => (
+            <BlurView style={StyleSheet.absoluteFill} intensity={60} />
+          ),
+          headerTransparent: true,
+          contentStyle: { backgroundColor: bg },
+        }}
+      />
+      <Stack.Screen
+        name="[id]"
+        options={{
+          gestureEnabled: true,
+          gestureDirection: "vertical",
+          enableTransitions: true,
+          screenStyleInterpolator: ({
+            layouts: {
+              screen: { width, height },
+            },
+            progress,
+            current,
+            insets,
+          }) => {
+            "worklet";
+
+            const scale = interpolate(progress, [0, 0.8, 1, 2], [0, 1, 1, 1]);
+            const borderRadius = interpolate(progress, [0, 1, 2], [2000, 0, 0]);
+
+            const translateY = interpolate(
+              current.gesture.y + insets.top + insets.bottom,
+              [0, height / 3, height],
+              [0, 0, height]
+            );
+
+            const yThreshold = height / 3;
+            const currentY = current.gesture.y + insets.top + insets.bottom;
+            const hasPassedThreshold = currentY > yThreshold;
+
+            let translateX = 0;
+
+            if (hasPassedThreshold) {
+              const excessY = currentY - yThreshold;
+              const maxExcessY = height - yThreshold;
+
+              translateX =
+                interpolate(
+                  current.gesture.x,
+                  [-width / 2, 0, width / 2],
+                  [-width, 0, width],
+                  "clamp"
+                ) * interpolate(excessY, [0, maxExcessY], [0, 1], "clamp");
+            }
+            return {
+              contentStyle: {
+                transform: [{ scale }, { translateY }, { translateX }],
+                borderRadius,
+              },
+            };
+          },
+          transitionSpec: {
+            close: Transition.specs.DefaultSpec,
+            open: Transition.specs.DefaultSpec,
+          },
+          // ...Transition.presets.ZoomIn(),
         }}
       />
     </Stack>
