faa2d593be8d6c11a7c1fb85cfc27ee3a0d39514
Solarin-Johnson <solarinjohnson@gmail.com>
2025-11-28 14:55:55 +0100

    feat: add HeadComponent for improved SEO and social media sharing with Open Graph and Twitter meta tags

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,11 +32,7 @@ export default function RootLayout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      {isWeb && (
-        <Head>
-          <meta name="color-scheme" content="light dark" />
-        </Head>
-      )}
+      {isWeb && <HeadComponent />}
       <KeyboardProvider>
         <SafeAreaProvider>
           <ThemeProvider
@@ -195,5 +191,45 @@ export function NavigationDrawer() {
         }}
       />
     </Drawer>
+  );
+}
+
+function HeadComponent() {
+  return (
+    <Head>
+      <title>My App</title>
+      <meta
+        name="description"
+        content="My personal Expo playground for React Native animations and UI experiments"
+      />
+      <meta name="color-scheme" content="light dark" />
+      <meta name="author" content="Solarin Johnson" />
+
+      {/* Open Graph / Facebook */}
+      <meta
+        name="og:image"
+        content="https://raw.githubusercontent.com/Solarin-Johnson/my-app/refs/heads/main/assets/images/twitter-cover.png"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="675" />
+      <meta property="og:title" content="My App" />
+      <meta
+        property="og:description"
+        content="My personal Expo playground for React Native animations and UI experiments"
+      />
+      <meta property="og:url" content="https://solarin.expo.app" />
+
+      {/* Twitter */}
+      <meta
+        name="twitter:image"
+        content="https://raw.githubusercontent.com/Solarin-Johnson/my-app/refs/heads/main/assets/images/twitter-cover.png"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="My App" />
+      <meta
+        name="twitter:description"
+        content="My personal Expo playground for React Native animations and UI experiments"
+      />
+    </Head>
   );
 }
