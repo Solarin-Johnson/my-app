6b99edcc6fa2033fa1d5eca1f120985380cd169a
Solarin-Johnson <solarinjohnson@gmail.com>
2026-07-13 02:08:41 +0100

    feat: enhance Title component with Description rendering and improved layout

--- components/change-mood/title.tsx
+++ components/change-mood/title.tsx
@@ -9,17 +9,15 @@ import {
 } from "./provider";
 import Animated, {
   cancelAnimation,
-  useAnimatedProps,
   useAnimatedReaction,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
-  withDelay,
   withSpring,
   withTiming,
 } from "react-native-reanimated";
 import { tintHex } from "@/functions";
-import { BlurView } from "expo-blur";
+import { ThemedText } from "../ThemedText";
 
 type TitleProps = {
   titles: string[];
@@ -28,6 +26,7 @@ type TitleProps = {
 
 // const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
 const IDLE_DURATION = 1500;
+const WELCOME_IDLE_DURATION = 5000;
 
 const applySpring = (value: number) => {
   "worklet";
@@ -78,31 +77,82 @@ export default function Title({ titles, colors }: TitleProps) {
   });
 
   return (
-    <View style={styles.container}>
-      <Animated.View style={animatedStyle}>
-        <UIConditionalRender
-          currentIndex={index}
-          style={styles.wrapper}
-          animationType="spring"
-          sharedAnimationConfig={sharedAnimationConfig}
-          elements={titles.map((title, index) => (
-            <Text
-              key={index}
-              style={[styles.text, { color: tintHex(colors[index], 0.5) }]}
-            >
-              {title}
-            </Text>
-          ))}
-        />
-      </Animated.View>
-      {/* <AnimatedBlurView
+    <>
+      <View style={styles.container}>
+        <Animated.View style={animatedStyle}>
+          <UIConditionalRender
+            currentIndex={index}
+            style={styles.wrapper}
+            animationType="spring"
+            sharedAnimationConfig={sharedAnimationConfig}
+            elements={titles.map((title, index) => (
+              <Text
+                key={index}
+                style={[styles.text, { color: tintHex(colors[index], 0.5) }]}
+              >
+                {title}
+              </Text>
+            ))}
+          />
+        </Animated.View>
+        {/* <AnimatedBlurView
         style={[StyleSheet.absoluteFill, styles.blur, blurAnimatedStyle]}
         tint="regular"
         animatedProps={blurAnimatedProp}
-      /> */}
-    </View>
+        /> */}
+      </View>
+      <Description />
+    </>
   );
 }
+
+const Description = () => {
+  const { currentIndex } = useChangeMood();
+  const timer = useSharedValue(0);
+  const show = useSharedValue(true);
+
+  useAnimatedReaction(
+    () => currentIndex.value,
+    (index, previousIndex) => {
+      cancelAnimation(timer);
+
+      if (index === 0 && (previousIndex == null || previousIndex === 0)) {
+        show.value = true;
+        return;
+      }
+
+      show.value = false;
+
+      if (index === 0) {
+        timer.value = 0;
+        timer.value = withTiming(
+          1,
+          { duration: WELCOME_IDLE_DURATION },
+          (finished) => {
+            if (finished) show.value = true;
+          },
+        );
+      }
+    },
+  );
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { scale: withSpring(show.value ? 1 : 0.75, SPRING_CONFIG_SLOW) },
+      ],
+      opacity: withSpring(show.value ? 1 : 0, SPRING_CONFIG_FAST),
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.descContainer, animatedStyle]}>
+      <ThemedText style={styles.descText} colorName="white">
+        How are you feeeling?
+      </ThemedText>
+    </Animated.View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -116,12 +166,26 @@ const styles = StyleSheet.create({
   },
   text: {
     fontFamily: "ui-rounded",
-    fontSize: 24,
+    fontSize: 21,
     fontWeight: "500",
   },
   blur: {
     zIndex: 0,
     left: 54,
     right: 54,
+  },
+  descContainer: {
+    marginBottom: 180,
+    position: "absolute",
+    // backgroundColor: "red",
+    width: "100%",
+    alignItems: "center",
+  },
+  descText: {
+    fontSize: 21,
+    letterSpacing: -0.2,
+    opacity: 0.75,
+    fontFamily: "ui-rounded",
+    fontWeight: "400",
   },
 });
