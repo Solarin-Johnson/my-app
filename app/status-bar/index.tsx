c1ee414149584814a0637a7f01e526fdaee1d0ad
Solarin-Johnson <solarinjohnson@gmail.com>
2026-07-28 17:48:13 +0100

    feat: add expo-localization dependency and integrate 24-hour clock format in StatusBarUI

--- app/status-bar/index.tsx
+++ app/status-bar/index.tsx
@@ -1,14 +1,16 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
 import StatusBarUI from "@/components/status-bar-ui";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import { getCalendars } from "expo-localization";
+
+const is24Hour = getCalendars()[0].uses24hourClock;
 
 export default function StatusBarPage() {
   const textColor = useThemeColor("text");
   return (
     <>
       <StatusBarUI
-        timeFormat="24"
+        timeFormat={is24Hour ? "24" : "12"}
         itemStyle={{ color: textColor, fontSize: 20 }}
         batterySize={38}
       />
